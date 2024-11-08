ea2db51b1c78025febb830d90fcd1f71a74d70a5
ranpy13 <85052085+ranpy13@users.noreply.github.com>
2024-11-08 13:04:38 +0530

    setup values

--- main.r
+++ main.r
@@ -1,3 +1,11 @@
 # install.packages("keras")
 
 # HandWritten digit recognition using Convolution Neural Networks
+data <- read.csv("train.csv")
+
+# Exploring the data
+dim(data)
+head(data[1:6])
+unique(unlist(data[1]))
+min(data[2:785])
+max(data[2:785])
