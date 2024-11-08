e4817c63b2c236e13ee6adf6b1069e81c2b66bd0
ranpy13 <85052085+ranpy13@users.noreply.github.com>
2024-11-08 13:13:53 +0530

    testing setup

--- main.r
+++ main.r
@@ -9,3 +9,10 @@ head(data[1:6])
 unique(unlist(data[1]))
 min(data[2:785])
 max(data[2:785])
+
+# Look at two samples, say the 4th and 7th images
+sample_4 <- matrix(as.numeric(data[4, -1]), nrow = 28, byrow = TRUE)
+image(sample_4, col = grey.colors(255))
+
+sample_7 <- matrix(as.numeric(data[7, -1]), nrow = 28, byrow = TRUE)
+image(sample_7, col = grey.colors(255))
