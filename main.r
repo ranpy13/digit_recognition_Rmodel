23a8031bec81399788efa30def808c6717555d52
ranpy13 <85052085+ranpy13@users.noreply.github.com>
2024-11-09 02:45:56 +0530

    multi layer nn

--- main.r
+++ main.r
@@ -115,4 +115,29 @@ data_train.y <- data_train[, -1]
 
 data <- mx.symbol.Variable("data")
 fc1 <- mx.symbol.FullyConnected(data, name = "fc1", num_hidden = 128)
-act1 <- mx.symbol.Activation("relu")
+act1 <- mx.symbol.Activation(fc1, name = "relu1", act_type = "relu")
+fc2 <- mx.symbol.FullyConnected(act1, name = "fc2", num_hidden = 64)
+act2 <- mx.symbol.Activation(fc2, name = "relu2", act_type = "relu")
+fc3 <- mx.symbol.FullyConnected(act2, name = "fc3", num_hidden = 10)
+softmax <- mx.symbol.SoftmaxOuput(fc3, name = "sm")
+
+devices <- mx.cpu()
+
+mx.set.seed(42)
+model_dnn <- mx.model.FeedForward.create(softmax,
+    X = data_tarin.x,
+    y = data_train.y,
+    ctx = devices,
+    num.round = 30,
+    array.batch.size = 100,
+    learning.rate = 0.01,
+    momentum = 0.9,
+    eval.metric = mx.metrix.accuracy,
+    initializer = mx.init.uniform(0.1),
+    epoch.end.callback = mx.callback.log.train.metric(100)
+)
+
+data_test.x <- data_test[, -1]
+data_test.x <- t(data_test.x / 255)
+prob_dnn <- predict(model_dnn, data_test.x)
+cm_dnn <- table(data$label, )
